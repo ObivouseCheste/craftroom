5267885252d582d507b4c38766b81a1f54791a55
asockman <agrippakellum@gmail.com>
2014-07-25 13:34:30 -0500

    gamelogic and PEP fixes

--- lobby.py
+++ lobby.py
@@ -3,7 +3,7 @@ from pyglet.window import key
 import random
 import cmenudp
 import socketserver
-import numpy
+import numpy as np
 import queue
 import datapacket
 
@@ -17,8 +17,8 @@ class Lobby(cmenudp.CmenClient, pyglet.window.Window):
         self.keys = pyglet.window.key.KeyStateHandler()
         self.push_handlers(self.keys)
         self.icon = pyglet.image.load("sprite.png")
-        self.sprites = dict()
-        self.sprite = Lobby.create_sprite(img=self.icon)
+        self.objects = dict()
+        self.objects['me'] = Lobby.create_sprite(img=self.icon)
         self.sq = queue.Queue()
         self.cconfirmed = False
         self.connectseed = bytes([random.randint(0, 255), random.randint(0, 255), random.randint(0, 255),
@@ -40,51 +40,49 @@ class Lobby(cmenudp.CmenClient, pyglet.window.Window):
                 elif msg.msg != self.connectseed:
                     print("Created:")
                     print(msg.uid)
-                    self.sprites[msg.uid] = Lobby.create_sprite(img = self.icon)
-            elif msg.uid in self.sprites:
-                pos = numpy.fromstring(msg.msg, dtype=float)
+                    self.objects[msg.uid] = Lobby.create_sprite(img=self.icon)
+            elif msg.uid in self.objects:
+                pos = np.fromstring(msg.msg, dtype=float)
                 #print(msg.uid)
                 #print(pos)
-                self.sprites[msg.uid].position = pos
+                self.objects[msg.uid].position = pos
 
         if self.cconfirmed:
             updated = False
             if self.keys[key.RIGHT]:
-                self.sprite.x += 80.0 * dt
+                self.objects['me'].x += 80.0 * dt
                 updated = True
             if self.keys[key.LEFT]:
-                self.sprite.x += -80.0 * dt
+                self.objects['me'].x += -80.0 * dt
                 updated = True
             if self.keys[key.UP]:
-                self.sprite.y += 80.0 * dt
+                self.objects['me'].y += 80.0 * dt
                 updated = True
             if self.keys[key.DOWN]:
-                self.sprite.y += -80.0 * dt
+                self.objects['me'].y += -80.0 * dt
                 updated = True
             if self.keys[key.SPACE]:
-                self.sprite.position = (0,0)
+                self.objects['me'].position = (0, 0)
                 updated = True
             if updated:
-                #print(self.sprite.position)
-                self.send(numpy.array([self.sprite.x, self.sprite.y]).tostring(), uid=self.uid)
+                #print(self.objects['me'].position)
+                self.send(np.array([self.objects['me'].x, self.objects['me'].y]).tostring(), uid=self.uid)
         else:
             self.time_since_attempt += dt
             if self.time_since_attempt >= 1:
                 self.time_since_attempt = 0
                 self.send(self.connectseed, connectTransaction=True)
 
-
     def on_draw(self):
         pyglet.clock.tick()
         self.clear()
         self.fps_display.draw()
         if self.cconfirmed:
-            self.sprite.draw()
-            for id, sprite in self.sprites.items():
+            self.objects['me'].draw()
+            for id, sprite in self.objects.items():
                 sprite.draw()
         else:
             self.connecting_label.draw()
-
 
     def received(self):
         while True:
@@ -94,12 +92,12 @@ class Lobby(cmenudp.CmenClient, pyglet.window.Window):
                 break
 
     @classmethod
-    def create_sprite(self, red=None, green=None, blue=None, x=50, y=50, img=None):
-        if red == None:
+    def create_sprite(cls, red=None, green=None, blue=None, x=50, y=50, img=None):
+        if red is None:
             red = random.randint(0, 255)
-        if green == None:
+        if green is None:
             green = random.randint(0, 255)
-        if blue == None:
+        if blue is None:
             blue = random.randint(0, 255)
         sprite = pyglet.sprite.Sprite(img, x, y)
         sprite.color = (red, green, blue)
@@ -114,6 +112,6 @@ class LobbyHandler(socketserver.BaseRequestHandler):
 
 if __name__ == "__main__":
     window = Lobby(("0.0.0.0", 12801), LobbyHandler)
-    window.connect("184.66.98.2", 12800)
+    window.connect("0.0.0.0", 12800)
     window.run()
     pyglet.app.run()
