d7033b0a5dd6fa7cec55a377225fe99dfa037af2
Wizzeh <me@wizzeh.com>
2014-07-26 20:56:41 -0700

    Moved to decorator system

--- gamelogic.py
+++ gamelogic.py
@@ -3,12 +3,13 @@ import collide
 import pyglet
 from lobby import Lobby, LobbyHandler
 from pyglet.window import key
+import random
 
 class World:
     def __init__(self):
         #maybe should be using sparse matrix
         self.keyobjs = {}
-        self.objects = set()
+        self.objs = set()
 
 
 
@@ -24,53 +25,33 @@ class SpaceLobby(Lobby):
         super().__init__(*args, **kwargs)
         self.tile = pyglet.image.load("block.png")
         self.world = World()
+        self.icon = pyglet.image.load("sprite.png")
+        self.world.keyobjs['me'] = Lobby.create_sprite(img=self.icon)
 
-    def update(self, dt):
-        for msg in self.received():
-            if msg.connectTransaction:
-                if not self.cconfirmed and msg.msg == self.connectseed:
-                    self.uid = msg.uid
-                    print(msg.uid)
-                    self.cconfirmed = True
-                    print("Got one")
-                elif msg.msg != self.connectseed:
-                    print("Created:")
-                    print(msg.uid)
-                    self.objects[msg.uid] = Lobby.create_sprite(img=self.icon, red=msg.msg[0], green=msg.msg[1],
+    def connect_client(self, msg):
+        self.objects[msg.uid] = Lobby.create_sprite(img=self.icon, red=msg.msg[0], green=msg.msg[1],
                                                                 blue=msg.msg[2])
-            elif msg.uid in self.objects:
-                pos = np.fromstring(msg.msg, dtype=float)
-                print(pos)
-                #print(msg.uid)
-                #print(pos)
-                self.objects[msg.uid].position = pos
-
-        if self.cconfirmed:
-            updated = False
-            if self.keys[key.RIGHT]:
-                self.objects['me'].x += 80.0 * dt
-                updated = True
-            if self.keys[key.LEFT]:
-                self.objects['me'].x += -80.0 * dt
-                updated = True
-            if self.keys[key.UP]:
-                self.objects['me'].y += 80.0 * dt
-                updated = True
-            if self.keys[key.DOWN]:
-                self.objects['me'].y += -80.0 * dt
-                updated = True
-            if self.keys[key.SPACE]:
-                self.objects['me'].x = 0.0
-                self.objects['me'].y = 0.0
-                updated = True
-            if updated:
-                #print(self.objects['me'].position)
-                self.send(np.array([self.objects['me'].x, self.objects['me'].y]).tostring(), uid=self.uid)
-        else:
-            self.time_since_attempt += dt
-            if self.time_since_attempt >= 1:
-                self.time_since_attempt = 0
-                self.send(self.connectseed, connectTransaction=True)
+    def logic(self, dt):
+        updated = False
+        if self.keys[key.RIGHT]:
+            self.objects['me'].x += 80.0 * dt
+            updated = True
+        if self.keys[key.LEFT]:
+            self.objects['me'].x += -80.0 * dt
+            updated = True
+        if self.keys[key.UP]:
+            self.objects['me'].y += 80.0 * dt
+            updated = True
+        if self.keys[key.DOWN]:
+            self.objects['me'].y += -80.0 * dt
+            updated = True
+        if self.keys[key.SPACE]:
+            self.objects['me'].x = 0.0
+            self.objects['me'].y = 0.0
+            updated = True
+        if updated:
+            #print(self.objects['me'].position)
+            self.send(np.array([self.objects['me'].x, self.objects['me'].y]).tostring(), uid=self.uid)
 
     def place_tile(self, x, y):
         self.world.objects += WorldObject(self.tile, x, y)
@@ -81,6 +62,18 @@ class SpaceLobby(Lobby):
     def on_mouse_press(self, x, y, button, modifiers):
         if self.me.dist_squared(x, y) < 900:
             self.place_tile(x, y)
+            
+    @classmethod
+    def create_sprite(cls, red=None, green=None, blue=None, x=50, y=50, img=None):
+        if red is None:
+            red = random.randint(0, 255)
+        if green is None:
+            green = random.randint(0, 255)
+        if blue is None:
+            blue = random.randint(0, 255)
+        sprite = pyglet.sprite.Sprite(img, x, y)
+        sprite.color = (red, green, blue)
+        return sprite
 
 if __name__ == "__main__":
     window = Lobby(("0.0.0.0", 12801), LobbyHandler)
--- lobby.py
+++ lobby.py
@@ -6,6 +6,7 @@ import socketserver
 import numpy as np
 import queue
 import datapacket
+import hashlib
 
 
 class Lobby(cmenudp.CmenClient, pyglet.window.Window):
@@ -17,12 +18,10 @@ class Lobby(cmenudp.CmenClient, pyglet.window.Window):
         self.fps_display = pyglet.clock.ClockDisplay()
         self.keys = pyglet.window.key.KeyStateHandler()
         self.push_handlers(self.keys)
-        self.icon = pyglet.image.load("sprite.png")
-        self.objects = dict()
-        self.objects['me'] = Lobby.create_sprite(img=self.icon)
+
         self.sq = queue.Queue()
         self.cconfirmed = False
-        self.connectseed = bytes([self.objects['me'].color[0], self.objects['me'].color[1], self.objects['me'].color[2]])
+        self.connectseed = bytes([random.randrange(256),random.randrange(256)])
         self.connecting_label = pyglet.text.Label('Connecting..', font_name='Times New Roman', font_size=36,
                                                   x=self.width // 2, y=self.height // 2, anchor_x='center',
                                                   anchor_y='center')
@@ -40,8 +39,8 @@ class Lobby(cmenudp.CmenClient, pyglet.window.Window):
                 elif msg.msg != self.connectseed:
                     print("Created:")
                     print(msg.uid)
-                    self.objects[msg.uid] = Lobby.create_sprite(img=self.icon, red=msg.msg[0], green=msg.msg[1],
-                                                                blue=msg.msg[2])
+                    self.connect_client(msg)
+
             elif msg.uid in self.objects:
                 pos = np.fromstring(msg.msg, dtype=float)
                 print(pos)
@@ -50,31 +49,19 @@ class Lobby(cmenudp.CmenClient, pyglet.window.Window):
                 self.objects[msg.uid].position = pos
 
         if self.cconfirmed:
-            updated = False
-            if self.keys[key.RIGHT]:
-                self.objects['me'].x += 80.0 * dt
-                updated = True
-            if self.keys[key.LEFT]:
-                self.objects['me'].x += -80.0 * dt
-                updated = True
-            if self.keys[key.UP]:
-                self.objects['me'].y += 80.0 * dt
-                updated = True
-            if self.keys[key.DOWN]:
-                self.objects['me'].y += -80.0 * dt
-                updated = True
-            if self.keys[key.SPACE]:
-                self.objects['me'].x = 0.0
-                self.objects['me'].y = 0.0
-                updated = True
-            if updated:
-                #print(self.objects['me'].position)
-                self.send(np.array([self.objects['me'].x, self.objects['me'].y]).tostring(), uid=self.uid)
+            self.logic(dt)
+    
         else:
             self.time_since_attempt += dt
             if self.time_since_attempt >= 1:
                 self.time_since_attempt = 0
                 self.send(self.connectseed, connectTransaction=True)
+
+    def connect_client(self, msg):
+        pass
+   
+    def logic(self, dt):
+        pass
 
     def on_draw(self):
         pyglet.clock.tick()
@@ -95,25 +82,18 @@ class Lobby(cmenudp.CmenClient, pyglet.window.Window):
                 break
 
     @classmethod
-    def create_sprite(cls, red=None, green=None, blue=None, x=50, y=50, img=None):
-        if red is None:
-            red = random.randint(0, 255)
-        if green is None:
-            green = random.randint(0, 255)
-        if blue is None:
-            blue = random.randint(0, 255)
-        sprite = pyglet.sprite.Sprite(img, x, y)
-        sprite.color = (red, green, blue)
-        return sprite
-
-    @classmethod
-    def propagated(cls, id):
+    def propagated(cls, id, encoder):
+        h = hashlib.md5()
+        h.update(id)
+        id = h.digest()[:3]
         def propagated_dec(method):
+            if id in cls.method_dict:
+                raise ValueError(id+" COLLIDED!!!!!!!!!!!!!!!!!!!!!!!")
             cls.method_dict[id] = method
 
             def prop_method(instance, *args, **kwargs):
                 method(*args, **kwargs)
-                instance.send(ENCODETHISSTUFF(id, *args, **kwargs))
+                instance.send(id+encoder(*args, **kwargs))
             return prop_method
         return propagated_dec
 
